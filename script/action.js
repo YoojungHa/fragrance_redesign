867581b61596abd0214a4e9bdf630732159b6663
YoojungHa <gkdbwjd39@gmail.com>
2024-09-23 16:43:45 +0900

    final

--- script/action.js
+++ script/action.js
@@ -6,3 +6,17 @@ $('.gnb li a').each(function() {
         $(this).parent().addClass('on')
     }
 })
+
+function row_scroll() {
+    document.querySelector(".gnb").addEventListener("wheel", (e) => {
+      e.preventDefault();
+
+      // deltaY를 사용하여 수직 휠 스크롤 값을 가져옴
+      var scrollAmount = e.deltaY;
+
+      // 현재 타겟 요소의 scrollLeft 값을 업데이트
+      e.currentTarget.scrollLeft += scrollAmount;
+    });
+  }
+
+  row_scroll();
